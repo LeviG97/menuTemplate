7de9c7e5bdd7a1e996c508cbd294527ad826acfa
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2022-04-27 20:23:26 +0100

    Add files via upload

---
+++ menuTemplate/menuTemplate/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace menuTemplate
+{
+    class Menu
+    {
+        string[] menu;
+        int choice;
+
+        public Menu(string[] options)
+        {
+            menu = options;
+        }
+
+        public int GetMenuChoice()
+        {
+            int menuChoice = 0;
+            do
+            {
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}.{menu[i]}");
+                }
+                Console.WriteLine("Enter Choice");
+
+                while (!int.TryParse(Console.ReadLine(), out menuChoice))
+                {
+                    Console.WriteLine("Please Enter a valid number: ");
+                }
+
+                if (menuChoice > menu.Length || menuChoice < 1)
+                {
+                    Console.WriteLine("Please Enter a Valid Choice: ");
+                }
+            }
+            while (menuChoice > menu.Length || menuChoice < 1);
+
+            return menuChoice;
+        }
+    }
+}
---
+++ menuTemplate/menuTemplate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace menuTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //menu
+            string[] mainMenuOptions = { "Option1", "Option2", "Option3", "Exit" };
+            Menu mainMenu = new Menu(mainMenuOptions);
+            int choice = 0;
+
+            do
+            {
+                choice = mainMenu.GetMenuChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Frist option");
+                        break;
+                    case 2:
+                        Console.WriteLine("Second option");
+                        break;
+                    case 3:
+                        Console.WriteLine("Third option");
+                        break;
+                    case 4:
+                        Console.WriteLine("Goodbye");
+                        break;
+                    default: break;
+                }
+            }
+            while (choice != 4);
+        }
+    }
+}
